3c38d9236a7b9b14a9915e58a44b1131e0d78a31
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-03-02 14:25:01 +0200

    Добавьте файлы проекта.

---
+++ _02_03_2020_TeamCoding_Hierarchy_Shapes_/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_03_2020_TeamCoding_Hierarchy_Shapes_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
