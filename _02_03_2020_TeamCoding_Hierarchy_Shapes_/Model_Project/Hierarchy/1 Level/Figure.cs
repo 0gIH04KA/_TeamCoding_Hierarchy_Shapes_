14c808f21ae00adb081877816de01b8216edfdfa
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-03-02 15:02:58 +0200

    Add New Region in the Class Figure

--- _02_03_2020_TeamCoding_Hierarchy_Shapes_/Model_Project/Hierarchy/1 Level/Figure.cs
+++ _02_03_2020_TeamCoding_Hierarchy_Shapes_/Model_Project/Hierarchy/1 Level/Figure.cs
@@ -8,5 +8,20 @@ namespace _02_03_2020_TeamCoding_Hierarchy_Shapes_
 {
     abstract class Figure
     {
+        #region ---===    Protected    ===---
+
+        #endregion
+
+        #region ---===    Get / Set    ===---
+
+        #endregion
+
+        #region ---===    Constructor    ===---
+
+        #endregion
+
+        #region ---===    Methods    ===---
+
+        #endregion
     }
 }
