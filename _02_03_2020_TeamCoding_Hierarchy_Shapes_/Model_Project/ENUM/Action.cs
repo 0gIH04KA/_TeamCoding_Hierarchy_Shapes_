487a8bf19c39cb81873e9fc9b7ae384496fd7b02
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-03-02 14:51:31 +0200

    Add New Enum Action in the Folder

---
+++ _02_03_2020_TeamCoding_Hierarchy_Shapes_/Model_Project/ENUM/Action.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_03_2020_TeamCoding_Hierarchy_Shapes_
+{
+    public enum Action
+    {
+        NoAction = 0x00,
+
+        ResizeIncrease = 0x01,
+        ResizeReduction = 0x02,
+
+        Tabulation = 0x04,
+
+        PressExit = 0xFFFF,
+    }
+}
