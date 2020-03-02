98f15918f8d8b4eed874128cea907b05b8cf7995
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-03-02 14:40:34 +0200

    Add New Class: MyException in the Folder

---
+++ _02_03_2020_TeamCoding_Hierarchy_Shapes_/Model_Project/Exception/MyException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_03_2020_TeamCoding_Hierarchy_Shapes_
+{
+    class MyException : Exception
+    {
+        public MyException()
+        {
+        }
+
+        public MyException(string message) : base(message)
+        {
+        }
+
+        public MyException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
