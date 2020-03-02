fcbbcca11f6f4bf97f51c9192c9cbf1bfa4a7383
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-03-02 14:52:38 +0200

    Add New Enum Direction in the Folder

---
+++ _02_03_2020_TeamCoding_Hierarchy_Shapes_/Model_Project/ENUM/Direction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_03_2020_TeamCoding_Hierarchy_Shapes_
+{
+    [Flags]
+    public enum Direction
+    {
+        NoAction = 0x00,
+
+        Right = 0x01,
+        Left = 0x02,
+        Up = 0x04,
+        Down = 0x08,
+
+        MoveAction = Right | Left | Up | Down,
+    }
+}
