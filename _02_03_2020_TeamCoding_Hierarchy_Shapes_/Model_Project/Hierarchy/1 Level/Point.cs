558f874ec94807d376c0dac2d0d088f04322bf50
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-03-02 15:29:55 +0200

    Add New Class Point in the Folder Hierarchy -> 1 level

---
+++ _02_03_2020_TeamCoding_Hierarchy_Shapes_/Model_Project/Hierarchy/1 Level/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_03_2020_TeamCoding_Hierarchy_Shapes_
+{
+    class Point
+    {
+        #region ---===    Private    ===---
+
+        private int _positionX;
+        private int _positionY;
+
+        #endregion
+
+        #region ---===    Get / Set    ===---
+
+
+
+        #endregion
+
+        #region ---===    Constructor    ===---
+
+        #endregion
+    }
+}
