3ddc59df180491b50da12c2f84ecd4f29b4c3cf1
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-03-26 23:22:08 +0200

    Welcome to Program)

--- _02_03_2020_TeamCoding_Hierarchy_Shapes_/Program.cs
+++ _02_03_2020_TeamCoding_Hierarchy_Shapes_/Program.cs
@@ -10,6 +10,20 @@ namespace _02_03_2020_TeamCoding_Hierarchy_Shapes_
     {
         static void Main(string[] args)
         {
+            Point point1 = new Point(1, 1);
+            ConsoleColor old = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(point1.PosX, point1.PosY);
+            Console.WriteLine("*");
+            Console.ForegroundColor = old;
+
+            Point point2 = new Point(3, 3);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(point2.PosX, point2.PosY);
+            Console.WriteLine("*");
+            Console.ForegroundColor = old;
+
         }
     }
 }
